b4dcf3d62eaae108ad5349d049bee080828fa3a5
r-aozora <zytrahidayat11@gmail.com>
2023-09-05 21:02:27 +0700

    feat: dark mode

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,12 +1,17 @@
-// Nav Fixed
+// Nav Fixed & Back to Top
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('nav-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('nav-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -18,3 +23,30 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('menu-active');
     navMenu.classList.toggle('hidden');
 });
+
+window.addEventListener('click', function(e) {
+    if(e.target != menu && e.target != navMenu) {
+        menu.classList.remove('menu-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// Theme Toggle
+const themeToggle = document.querySelector('#theme-toggle');
+const html = document.querySelector('html');
+
+themeToggle.addEventListener('click', function(){
+    if (themeToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggle.checked = true;
+} else {
+    themeToggle.checked = false;
+}
