25a48cb1d01078e47607ab2011e8beaf6ee781a2
r-aozora <zytrahidayat11@gmail.com>
2024-05-19 17:12:40 +0700

    feat: update content part 2

--- dist/js/index.js
+++ dist/js/index.js
@@ -50,3 +50,23 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
 } else {
     themeToggle.checked = false;
 }
+
+// Contact Form Handler
+window.addEventListener("load", function () {
+    const form = document.getElementById('contact-form')
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault()
+
+        const data = new FormData(form)
+        const action = e.target.action
+
+        fetch(action, {
+            method: 'POST',
+            body: data,
+        })
+            .then(() => {
+                alert("Pesan terkirim!")
+            })
+    })
+})
